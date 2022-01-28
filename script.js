4e1d7c2e422a2941fb5099b5493e6b09d27e4a22
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-01-28 22:54:17 +0100

    Update

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let arrOfImg = [
-    "images/dice1.png",
-    "images/dice2.png",
-    "images/dice3.png",
-    `images/dice4.png`,
-    `images/dice5.png`,
-    `images/dice6.png`
-]
-
-const refresh =document.querySelector("h1");
+  "images/dice1.png",
+  "images/dice2.png",
+  "images/dice3.png",
+  `images/dice4.png`,
+  `images/dice5.png`,
+  `images/dice6.png`,
+];
+const refresh = document.querySelector("h1");
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
+const remark = document.querySelector(".remark");
+const players = document.querySelectorAll(".player");
+
+
+players.forEach((player,index) => {player.innerHTML=prompt(`Enter your name Player ${index + 1}`)});
+// console.log(players)
 
 
 refresh.addEventListener("click", () => {
-    let random1 = Math.floor(Math.random() * arrOfImg.length);
-    let random2 = Math.floor(Math.random() * arrOfImg.length);
-    img1.src = arrOfImg[random1];
-    img2.src = arrOfImg[random2];
+  let random1 = Math.floor(Math.random() * arrOfImg.length);
+  let random2 = Math.floor(Math.random() * arrOfImg.length);
+  img1.src = arrOfImg[random1];
+  img2.src = arrOfImg[random2];
+
+  random1 === random2
+    ? (remark.textContent = "Draw")
+    : random1 > random2
+    ? (remark.textContent = "Player 1 Wins")
+    : (remark.textContent = "Player 2 Wins");
+    
 });
